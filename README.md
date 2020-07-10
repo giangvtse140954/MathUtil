8d8f9a7af2a90713e941e0ab0a6fe3850a5bb0ae
giangvtse140954 <giangvtse140954@fpt.edu.vn>
2020-07-10 10:21:00 +0700

    1st: first upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# Welcome to giangvtse140954 repository
+This repository contains a simple Java 
+application used for the demonstration
+of CI - Continuous Integration process,
+as a part of Introduction to Software
+Engineering course at FPT University, HCMC Campus
+
+## You will find in this repo:
+* MathUtil class - holds all of my own math methods
+* Test class - uses to check the code quality (demo for 
+TDD and Unit Test technics)
+* .gitignore
+* Markdown code - this file itself
+
+## Connect me via
+1. [Facebook](https://facebook.com/giang.coi.104)
+2. [Github](https://github.com/giangvtse140954)
+
+#### © 2020 by giangvtse140954
---
+++ src/giang/util/MathUtil.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package giang.util;
+
+/**
+ *
+ * @author MY HP
+ */
+
+// đây là class chứa các hàm tính toán, toán học, dùng chung, làm tiện ích
+// cho các nơi khác sử dụng. Phàm cái gì mà mang tính chắc xài chung
+// ta dùng kĩ thuật STATIC
+public class MathUtil {
+    
+    // hàm tính n! = 1.2.3...n
+    // vì giá trị giai thừa tăng cực nhanh, nên sẽ tràn vùng int rất sớm
+    // do int chỉ tối đa 2 tỷ 1, nên ta xài long thì mới chứa đc n lớn
+    // nhưng cũng chỉ cỡ 15! thoai
+    public static long computeFactorial(int n) {
+        
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+    
+}

7d23b788372e219e5645a8693e63b8abde8e18dd
giangvtse140954 <giangvtse140954@fpt.edu.vn>
2020-07-20 10:03:15 +0700

    add the exception handler for the invalid argument checking

--- src/giang/util/MathUtil.java
+++ src/giang/util/MathUtil.java
@@ -20,7 +20,10 @@ public class MathUtil {
     // do int chỉ tối đa 2 tỷ 1, nên ta xài long thì mới chứa đc n lớn
     // nhưng cũng chỉ cỡ 15! thoai
     public static long computeFactorial(int n) {
-        
+        if (n < 0 || n > 15) {
+            throw new IllegalArgumentException("Invalid argument. n must be >= 0 "
+                    + "and <= 15");
+        }
         long result = 1;
         for (int i = 1; i <= n; i++) {
             result *= i;

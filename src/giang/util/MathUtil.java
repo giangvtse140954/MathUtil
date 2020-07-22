9b0a44df748dbcc5e8874b29739a5329d77f9ab6
giangvtse140954 <giangvtse140954@fpt.edu.vn>
2020-07-22 09:54:03 +0700

    refactor the cF() method by using the recursion technic

--- src/giang/util/MathUtil.java
+++ src/giang/util/MathUtil.java
@@ -9,12 +9,11 @@ package giang.util;
  *
  * @author MY HP
  */
-
 // đây là class chứa các hàm tính toán, toán học, dùng chung, làm tiện ích
 // cho các nơi khác sử dụng. Phàm cái gì mà mang tính chắc xài chung
 // ta dùng kĩ thuật STATIC
 public class MathUtil {
-    
+
     // hàm tính n! = 1.2.3...n
     // vì giá trị giai thừa tăng cực nhanh, nên sẽ tràn vùng int rất sớm
     // do int chỉ tối đa 2 tỷ 1, nên ta xài long thì mới chứa đc n lớn
@@ -24,11 +23,16 @@ public class MathUtil {
             throw new IllegalArgumentException("Invalid argument. n must be >= 0 "
                     + "and <= 15");
         }
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
+
+        if (n == 0 || n == 1) {
+            return 1;
         }
-        return result;
+        return n * computeFactorial(n - 1);
+//        long result = 1;
+//        for (int i = 1; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
     }
-    
+
 }
